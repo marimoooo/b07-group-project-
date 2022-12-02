086544e7b7fe8ec30006c7011ff92c6a1ff95fa4
wangz555 <114190129+wangz555@users.noreply.github.com>
2022-12-02 09:21:30 -0500

    Display activity

--- app/src/main/java/com/example/recyclerview2/Course.java
+++ app/src/main/java/com/example/recyclerview2/Course.java
@@ -4,9 +4,20 @@ public class Course {
     public String code;
     public String name;
 
+    public Course(){
+
+    }
+
     public Course(String code, String name){
         this.code = code;
         this.name = name;
     }
 
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
