6eda94a5910c42a4f0eeb82908add85fb26ad962
sally <66801747+notZhuo@users.noreply.github.com>
2022-12-02 16:39:15 -0500

    nm1

--- app/src/main/java/com/example/myapplication20/FirstFragment.java
+++ app/src/main/java/com/example/myapplication20/FirstFragment.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+//ok
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 //import androidx.navigation.fragment.NavHostFragment;
