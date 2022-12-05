c75b97dfbcbb0c62da09b2a9573240f7203998d8
sally <66801747+notZhuo@users.noreply.github.com>
2022-12-05 17:17:52 -0500

    When course is deleted the same course that is stored as a pre-requisite is also be deleted

--- app/src/main/java/com/example/b07/MainActivity3.java
+++ app/src/main/java/com/example/b07/MainActivity3.java
@@ -5,17 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class MainActivity3 extends AppCompatActivity {
@@ -23,8 +28,9 @@ public class MainActivity3 extends AppCompatActivity {
     Intent intent, intent2;
     DatabaseReference databaseRef;
     Button delete, backButton;
-    String username, code;
+    String username, code, newP;
     EditText newCourseName;
+    List<String> prereqList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,14 +57,58 @@ public class MainActivity3 extends AppCompatActivity {
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                             for(DataSnapshot snapshot: dataSnapshot.getChildren()){
                                 databaseRef.child("students").child(Objects.requireNonNull(snapshot.getKey())).child("courses").child(code).setValue(null);
-                                Toast.makeText(MainActivity3.this, code + " is deleted", Toast.LENGTH_SHORT).show();
-                                finish();
                             }
                         }
                         @Override
                         public void onCancelled(@NonNull DatabaseError error) {
                         }
                     });
+                    //delete course in course pre-req of other courses
+                    databaseRef.child("Course details").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                Log.d("myTag", "current course: " + snapshot.getKey());
+                                databaseRef.child("Course details").child(Objects.requireNonNull(snapshot.getKey())).child("prereq").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                        if (!task.isSuccessful()) {
+                                            Log.e("firebase", "Error getting data", task.getException());
+                                        } else {
+                                            Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                                            if (task.getResult().getValue(String.class).contains(code)){
+                                                String p = task.getResult().getValue(String.class);
+                                                Log.d("myTag", "current preq of " +snapshot.getKey()+ ": " + p);
+                                                prereqList = Arrays.asList(p.split(","));
+                                                newP="";
+                                                for( String s : prereqList){
+                                                    Log.d("myTag", "origional newP: " + newP);
+                                                    if(!s.equals(code)){
+                                                        newP+=s + ",";
+                                                        Log.d("myTag", "new newP: " + newP);
+                                                    }
+                                                }
+                                                Log.d("myTag", "final newP: " + newP);
+                                                if(newP.equals("")){
+                                                    databaseRef.child("Course details").child(Objects.requireNonNull(snapshot.getKey())).child("prereq").setValue("");
+                                                    Toast.makeText(MainActivity3.this, code + " is deleted", Toast.LENGTH_SHORT).show();
+                                                    //finish();
+                                                }else{
+                                                    databaseRef.child("Course details").child(Objects.requireNonNull(snapshot.getKey())).child("prereq").setValue(newP.substring(0,newP.length()-1));
+                                                    Toast.makeText(MainActivity3.this, code + " is deleted", Toast.LENGTH_SHORT).show();
+                                                    //finish();
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+
                 }
                 else{
                     Toast.makeText(MainActivity3.this, "code can't be empty", Toast.LENGTH_SHORT).show();
