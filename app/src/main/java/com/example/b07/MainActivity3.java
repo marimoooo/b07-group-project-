47820fc23c3efc4d59d28d26c44a7dcc589c19e8
Divyansh_Kachchhava <divyansh.kachchhava@mail.utoronto.ca>
2022-12-05 22:47:18 -0500

    Second Final version

--- app/src/main/java/com/example/b07/MainActivity3.java
+++ app/src/main/java/com/example/b07/MainActivity3.java
@@ -5,163 +5,118 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    Intent intent, intent2;
     DatabaseReference databaseRef;
+    Button delete, backButton;
+    String username, code, newP;
+    EditText newCourseName;
+    List<String> prereqList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-//        EditText newCourseName = findViewById(R.id.newCourseName);
-        final TextView edit_pre = findViewById(R.id.textView27);
-        final Button delete2 = findViewById(R.id.button12);
-        edit_pre.setText("");
-        final String[] m = {""};
         databaseRef = FirebaseDatabase.getInstance("https://course-planner-14-default-rtdb.firebaseio.com/").getReference();
-        Button delete = findViewById(R.id.button3);
-        Intent intent = getIntent();
-        String username = intent.getStringExtra("username");
+        newCourseName = findViewById(R.id.newCourseName);
+        backButton = findViewById(R.id.backButton);
+        delete = findViewById(R.id.button3);
+        intent = getIntent();
+        username = intent.getStringExtra("username");
 
-        String z[] = {""};
-        List<String> items2;
-        items2 = new ArrayList<>();
-        items2.add("--select--");
-        Spinner s2 = findViewById(R.id.dropdownmenuq5);
-        DatabaseReference reference = FirebaseDatabase.getInstance("https://course-planner-14-default-rtdb.firebaseio.com/").getReference().child("Course details");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
-                    items2.add(snapshot.getKey().toString());
-                    z[0] = z[0] + snapshot.getKey().toString();
-                    final TextView edit_pre = findViewById(R.id.textView27);
-//                    Toast.makeText(Course_addition_admin.this, "" + snapshot.getKey().toString() + "is added to the course pre-req.", Toast.LENGTH_SHORT).show();
-//                    list.add(snapshot.getValue().toString());
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        delete2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                edit_pre.setText("");
-//                Toast.makeText(Course_addition_admin.this, "Selected courses removed", Toast.LENGTH_SHORT).show();
-            }
-        });
-        if(items2 == null) {
-            // Do something
-        } else {
-            // Do something else
-            s2.setAdapter(new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, items2));
-        }
-        s2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-        String k[] = {""};
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String item = items2.get(i);
-                if(!Objects.equals(item, "--select--")){
-//                    if(!Objects.equals(k[0], "")) k[0] = k[0] + ",";
-                    m[0] = item;
-                    edit_pre.setText(item);
-
-//                    edit_pre.setText(k[0]);
-                    Toast.makeText(MainActivity3.this, "" + item + " is selected to delete.", Toast.LENGTH_SHORT).show();}
-                else{
-                    edit_pre.setText("");
-                    m[0] = "";
-                }
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        final Button backButton = findViewById(R.id.backButton);
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String code = m[0];
+                code = newCourseName.getText().toString();
                 //check if the code is empty
-                if (!(code.equals("") || code.equals("--select--"))) {
-                        Toast.makeText(MainActivity3.this, "" + code + " is deleted", Toast.LENGTH_SHORT).show();
-                        databaseRef.child("Course details").child("" + code + "").removeValue();
-                        startActivity(new Intent(MainActivity3.this, admin_main.class));
-                        finish();
-
-                        DatabaseReference reference = FirebaseDatabase.getInstance("https://course-planner-14-default-rtdb.firebaseio.com/").getReference().child("students");
-                        reference.addValueEventListener(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                                    databaseRef.child("students").child(Objects.requireNonNull(snapshot.getKey())).child("courses").child("" + code + "").removeValue();
-                                    finish();
-                                }
+                if(!code.equals("")){
+                    //delete course in admin
+                    databaseRef.child("Course details").child(code).setValue(null);
+                    Toast.makeText(MainActivity3.this, ""+ code + " is deleted", Toast.LENGTH_SHORT).show();
+                    databaseRef.child("Course details").child(""+code+"").removeValue();
+                    // startActivity(new Intent(MainActivity3.this, admin_main.class));
+                    Intent intent = new Intent(MainActivity3.this, admin_main.class);
+                    intent.putExtra("username", username);
+                    startActivity(intent);
+                    finish();
+                    //delete course in student
+                    databaseRef.child("students").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                                databaseRef.child("students").child(Objects.requireNonNull(snapshot.getKey())).child("courses").child(code).setValue(null);
                             }
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+                    //delete course in course pre-req of other courses
+                    databaseRef.child("Course details").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                Log.d("myTag", "current course: " + snapshot.getKey());
+                                databaseRef.child("Course details").child(Objects.requireNonNull(snapshot.getKey())).child("prereq").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                        if (!task.isSuccessful()) {
+                                            Log.e("firebase", "Error getting data", task.getException());
+                                        } else {
+                                            Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                                            if (task.getResult().getValue(String.class).contains(code)){
+                                                String p = task.getResult().getValue(String.class);
+                                                Log.d("myTag", "current preq of " +snapshot.getKey()+ ": " + p);
+                                                prereqList = Arrays.asList(p.split(","));
+                                                newP="";
+                                                for( String s : prereqList){
+                                                    Log.d("myTag", "origional newP: " + newP);
+                                                    if(!s.equals(code)){
+                                                        newP+=s + ",";
+                                                        Log.d("myTag", "new newP: " + newP);
+                                                    }
+                                                }
+                                                Log.d("myTag", "final newP: " + newP);
+                                                if(newP.equals("")){
+                                                    databaseRef.child("Course details").child(Objects.requireNonNull(snapshot.getKey())).child("prereq").setValue("");
+                                                    Toast.makeText(MainActivity3.this, code + " is deleted", Toast.LENGTH_SHORT).show();
+                                                    //finish();
+                                                }else{
+                                                    databaseRef.child("Course details").child(Objects.requireNonNull(snapshot.getKey())).child("prereq").setValue(newP.substring(0,newP.length()-1));
+                                                    Toast.makeText(MainActivity3.this, code + " is deleted", Toast.LENGTH_SHORT).show();
+                                                    //finish();
+                                                }
+                                            }
+                                        }
+                                    }
+                                });
                             }
-                        });
-
-//                    DatabaseReference ref = FirebaseDatabase.getInstance("https://course-planner-14-default-rtdb.firebaseio.com/").getReference().child("Course details");
-//                    ref.addValueEventListener(new ValueEventListener() {
-//                        @Override
-//                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                                String code = snapshot.getKey();
-//                                Toast.makeText(MainActivity3.this, code, Toast.LENGTH_SHORT).show();
-//                                DatabaseReference mostafa = FirebaseDatabase.getInstance("https://course-planner-14-default-rtdb.firebaseio.com/").getReference().child("Course details").child(code);
-//                                mostafa.addValueEventListener(new ValueEventListener() {
-//                                    @Override
-//                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-//                                            String list = Objects.requireNonNull(snapshot.getValue()).toString();
-//                                            Toast.makeText(MainActivity3.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
-//                                            Toast.makeText(MainActivity3.this, list, Toast.LENGTH_SHORT).show();
-//                                            list.replace("," + code, "");
-//                                            mostafa.setValue(list);
-//                                        }
-//                                    }
-//                                    @Override
-//                                    public void onCancelled(@NonNull DatabaseError error) {
-//
-//                                    }
-//                                });
-//                            }
-//                        }
-//                        @Override
-//                        public void onCancelled(@NonNull DatabaseError error) {
-//
-//                        }
-//                    });
-//
-                } else {
-                    Toast.makeText(MainActivity3.this, "please select some code", Toast.LENGTH_SHORT).show();
-                    edit_pre.setText("");
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                        }
+                    });
+                }else{
+                    Toast.makeText(MainActivity3.this, "code can't be empty", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -169,9 +124,9 @@ public class MainActivity3 extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity3.this, admin_main.class);
-                intent.putExtra("username", username);
-                startActivity(intent);
+                intent2 = new Intent(MainActivity3.this, admin_main.class);
+                intent2.putExtra("username", username);
+                startActivity(intent2);
             }
         });
     }
